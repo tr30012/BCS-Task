52669f044803ff091e9cd568da0a5998710decf3
tr30012 <72806811+tr30012@users.noreply.github.com>
2021-10-09 16:08:27 +0300

    s

--- bcschaintask/bcschaintask/settings.py
+++ bcschaintask/bcschaintask/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'bcschaintask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hqeoncxs',
+        'USER': 'hqeoncxs',
+        'PASSWORD': 'WdKLsyfCggkQL_49usFayR_2pQ7P7CtZ',
+        'HOST': 'hattie.db.elephantsql.com',
     }
 }
 
